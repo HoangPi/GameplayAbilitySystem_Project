241d36f93424b1b2ca8537cb8dac95fe1d6ab63a
Phi <voduchoangphi@gmail.com>
2025-06-16 16:07:48 +0700

    add cooldown tag for switch target

---
+++ Source/GASP6/Private/Tags/Ability/Lockon/TagLockon.cpp
@@ -0,0 +1,12 @@
+#include "Tags/Ability/Lockon/TagLockon.h"
+
+namespace MyTags
+{
+    namespace Ability
+    {
+        namespace Lockon
+        {
+            UE_DEFINE_GAMEPLAY_TAG(cooldown, "ability.lockon.cooldown")
+        }
+    }
+}
