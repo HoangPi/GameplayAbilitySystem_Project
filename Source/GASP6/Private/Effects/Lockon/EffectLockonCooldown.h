7988a71053f73095d042d046ee94e82450a840a6
Phi <voduchoangphi@gmail.com>
2025-06-16 14:38:53 +0700

    declare cooldown effect

---
+++ Source/GASP6/Private/Effects/Lockon/EffectLockonCooldown.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "EffectLockonCooldown.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEffectLockonCooldown : public UGameplayEffect
+{
+	GENERATED_BODY()
+	
+};
