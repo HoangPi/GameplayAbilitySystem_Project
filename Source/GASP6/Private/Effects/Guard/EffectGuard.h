263e8d300a7f123785dcfedfb698d40150ed064a
Phi <voduchoangphi@gmail.com>
2025-06-24 23:06:15 +0700

    declare effect

---
+++ Source/GASP6/Private/Effects/Guard/EffectGuard.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "EffectGuard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEffectGuard : public UGameplayEffect
+{
+	GENERATED_BODY()
+	
+};
