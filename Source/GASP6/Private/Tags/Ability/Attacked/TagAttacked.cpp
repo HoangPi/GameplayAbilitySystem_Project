622837420f747bddfb4f9b2106aa818b6e15448f
Phi <voduchoangphi@gmail.com>
2025-06-27 14:23:34 +0700

    Declare projectile tag

---
+++ Source/GASP6/Private/Tags/Ability/Attacked/TagAttacked.cpp
@@ -0,0 +1,12 @@
+#include "Tags/Ability/Attacked/TagAttacked.h"
+
+namespace MyTags
+{
+    namespace Ability
+    {
+        namespace Attacked
+        {
+            UE_DEFINE_GAMEPLAY_TAG(projectile, "ability.attacked.projectile")
+        }
+    }
+}
