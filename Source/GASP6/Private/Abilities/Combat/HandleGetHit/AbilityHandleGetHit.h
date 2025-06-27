857e6d61870509f3878b59b1d18f332732285aa6
Phi <voduchoangphi@gmail.com>
2025-06-27 14:26:27 +0700

    declare ability

---
+++ Source/GASP6/Private/Abilities/Combat/HandleGetHit/AbilityHandleGetHit.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "AbilityHandleGetHit.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UAbilityHandleGetHit : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
