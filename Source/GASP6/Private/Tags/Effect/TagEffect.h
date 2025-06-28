33ae51bb26f0129581fd605f672e5fbbaabee1f3
Phi <voduchoangphi@gmail.com>
2025-06-28 15:32:32 +0700

    add health effect tags

--- Source/GASP6/Private/Tags/Effect/TagEffect.h
+++ Source/GASP6/Private/Tags/Effect/TagEffect.h
@@ -3,11 +3,13 @@
 #include "NativeGameplayTags.h"
 
 #include "Tags/Effect/Stamina/TagEffectStamina.h"
+#include "Tags/Effect/Health/TagHealth.h"
 
 namespace MyTags
 {
     namespace Effect
     {
         UE_DECLARE_GAMEPLAY_TAG_EXTERN(stamina)
+        UE_DECLARE_GAMEPLAY_TAG_EXTERN(health)
     }
 }
