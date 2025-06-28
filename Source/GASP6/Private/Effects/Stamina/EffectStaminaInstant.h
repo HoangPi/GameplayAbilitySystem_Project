a32a7fb6d9e43bfc45815528b93b9bf26463ef6c
Phi <voduchoangphi@gmail.com>
2025-06-28 16:28:30 +0700

    declare stamina effect

---
+++ Source/GASP6/Private/Effects/Stamina/EffectStaminaInstant.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "EffectStaminaInstant.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEffectStaminaInstant : public UGameplayEffect
+{
+	GENERATED_BODY()
+	
+};
