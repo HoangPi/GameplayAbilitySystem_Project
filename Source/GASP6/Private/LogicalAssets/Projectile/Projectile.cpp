119e201a63caa0903b41a685461bd7b398deb11d
Phi <voduchoangphi@gmail.com>
2025-06-28 14:29:39 +0700

    HOMING MISSLE!!!

--- Source/GASP6/Private/LogicalAssets/Projectile/Projectile.cpp
+++ Source/GASP6/Private/LogicalAssets/Projectile/Projectile.cpp
@@ -1,14 +1,28 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "LogicalAssets/Projectile/Projectile.h"
+
+#include "Kismet/KismetMathLibrary.h"
+#include "Ultilities/MyMacros.h"
 
 // Sets default values
 AProjectile::AProjectile()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	this->SetLifeSpan(3.0f);
 
+	this->CollisionBox = this->CreateDefaultSubobject<UBoxComponent>("My_Collision_Box");
+	this->RootComponent = this->CollisionBox;
+	this->CollisionBox->SetBoxExtent(FVector(20.0f));
+	this->CollisionBox->SetCollisionObjectType(ECollisionChannel::ECC_EngineTraceChannel2);
+
+	this->ForwardArrow = this->CreateDefaultSubobject<UArrowComponent>("My_Foward_Arrow");
+	this->ForwardArrow->SetupAttachment(this->RootComponent);
+	this->ForwardArrow->SetWorldRotation(this->GetActorRotation());
+
+	this->Speed = 850.0f;
+	this->MaxTurningSpeed = 48.0f;
 }
 
 // Called when the game starts or when spawned
@@ -22,6 +36,35 @@ void AProjectile::BeginPlay()
 void AProjectile::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	if(this->WaitForTarget && !this->ProjectileTarget)
+	{
+		return;
+	}
+	if (this->ProjectileTarget)
+	{
+		this->UpdateProjectileRotation(DeltaTime);
+	}
+	this->SetActorLocation(
+		this->GetActorLocation() + this->ForwardArrow->GetForwardVector() * DeltaTime * this->Speed,
+		true
+	);
 }
 
+void AProjectile::UpdateProjectileRotation(float DeltaTime)
+{
+	FVector TargetLocation = this->ProjectileTarget->GetActorLocation();
+	FVector ActorLocation = this->GetActorLocation();
+
+	FRotator DesiredRotation = UKismetMathLibrary::FindLookAtRotation(ActorLocation, TargetLocation);
+	FRotator CurrentRotation = this->GetActorRotation();
+	FRotator Diff = UKismetMathLibrary::NormalizedDeltaRotator(DesiredRotation, CurrentRotation);
+	double min = -1 * this->MaxTurningSpeed * DeltaTime;
+	double max = this->MaxTurningSpeed * DeltaTime;
+	this->SetActorRotation(
+		FRotator(
+			CurrentRotation.Pitch + CLAMP(Diff.Pitch, max, min),
+			CurrentRotation.Yaw + CLAMP(Diff.Yaw, max, min),
+			CurrentRotation.Roll + CLAMP(Diff.Roll, max, min)
+		)
+	);
+}
--- Source/GASP6/Private/LogicalAssets/Projectile/Projectile.h
+++ Source/GASP6/Private/LogicalAssets/Projectile/Projectile.h
@@ -4,9 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+
+#include "Components/BoxComponent.h"
+#include "Components/ArrowComponent.h"
+#include "GameFramework/Actor.h"
+
 #include "Projectile.generated.h"
 
-UCLASS()
+UCLASS(Blueprintable)
 class AProjectile : public AActor
 {
 	GENERATED_BODY()
@@ -15,10 +20,29 @@ public:
 	// Sets default values for this actor's properties
 	AProjectile();
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Collision")
+	UBoxComponent *CollisionBox;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Direction")
+	UArrowComponent* ForwardArrow;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ProjectileBehavior")
+	AActor *ProjectileTarget;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "ProjectileBehavior")
+	double Speed;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "ProjectileBehavior")
+	double MaxTurningSpeed;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "ProjectileBehavior")
+	bool WaitForTarget = true;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	inline void UpdateProjectileRotation(float DeltaTime);
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
