704adf02917bf894a3f66c8c84527ea8dc6cb6d5
Phi <voduchoangphi@gmail.com>
2025-06-29 14:39:50 +0700

    declare task

---
+++ Source/GASP6/Private/Abilities/Tasks/AbilityTaskNotifyPlayerRecover.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask_WaitGameplayEffectRemoved.h"
+#include "AbilityTaskNotifyPlayerRecover.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UAbilityTaskNotifyPlayerRecover : public UAbilityTask_WaitGameplayEffectRemoved
+{
+	GENERATED_BODY()
+	
+};
