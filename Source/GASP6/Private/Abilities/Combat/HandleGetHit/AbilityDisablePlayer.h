d98b5d635baecb023a78507fc414d2cee8f5568e
Phi <voduchoangphi@gmail.com>
2025-06-30 11:39:28 +0700

    define ability

--- Source/GASP6/Private/Abilities/Combat/HandleGetHit/AbilityDisablePlayer.h
+++ Source/GASP6/Private/Abilities/Combat/HandleGetHit/AbilityDisablePlayer.h
@@ -4,14 +4,35 @@
 
 #include "CoreMinimal.h"
 #include "Abilities/GameplayAbility.h"
+
+#include "Abilities/Tasks/AbilityTask_WaitGameplayEffectRemoved.h"
+
 #include "AbilityDisablePlayer.generated.h"
 
 /**
  * 
  */
+
+DECLARE_DELEGATE(FOnNotifyPlayerDown);
+DECLARE_DELEGATE(FOnNotifyPlayerRecover);
+
 UCLASS()
 class UAbilityDisablePlayer : public UGameplayAbility
 {
 	GENERATED_BODY()
-	
+
+public:
+	UAbilityDisablePlayer();
+
+	FOnNotifyPlayerDown NotifyPlayerDown;
+	FOnNotifyPlayerRecover NotifyPlayerRecover;
+
+	void ActivateAbility(
+		FGameplayAbilitySpecHandle Handle, 
+		const FGameplayAbilityActorInfo *ActorInfo, 
+		FGameplayAbilityActivationInfo ActivationInfo, 
+		const FGameplayEventData *TriggerEventData);
+
+	UFUNCTION()
+	void NotifyPlayerRecoverHandler(const FGameplayEffectRemovalInfo & unused);
 };
